f49055b7f5b82ec314e90a03a7bebcaa65a551cd
fiorella <fiore22herrera@gmail.com>
2023-08-23 14:34:45 -0400

    Migrate tablas

--- database/migrations/2023_08_23_143315_create_mesa_tecnica_table.php
+++ database/migrations/2023_08_23_143315_create_mesa_tecnica_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('mesa_tecnica', function (Blueprint $table) {
+            $table->id()->startingValue(6100);
+            $table->string('nombre_mta');
+            $table->string('telefono_encargado')->nullable();
+            $table->string('correo_encargado')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mesa_tecnica');
     }
 };
