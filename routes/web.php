6bae4d72dce38dae622c1f722fef1124e1900228
Wilfredo Murillo <wilmugo@gmail.com>
2023-09-06 19:24:20 +0000

    agregado middlewares guest en rutas

--- routes/web.php
+++ routes/web.php
@@ -21,25 +21,26 @@ use Illuminate\Support\Facades\Route;
 // });
 // rutas principales y login
 Route::redirect('/', 'login');
-Route::get('/login', [UserController::class, 'create'])->name('login.index');
-Route::post('/login', [UserController::class, 'store'])->name('login.post');
-Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+Route::get('/login', [UserController::class, 'create'])->name('login.index')->middleware('guest');
+Route::post('/login', [UserController::class, 'store'])->name('login.post')->middleware('guest');
 
 
+//Routes auth
+Route::post('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
 Route::get('/homepage', [UserController::class, 'homepage'])->name('home')->middleware('auth');
 Route::post('/solicitud', [SolicitudController::class, 'store'])->name('solicitud')->middleware('auth');
 
 
 //Routes de Usuario
-Route::get('/registro', [RegistroController::class, 'create'])->name('registro');
-Route::post('/registro', [RegistroController::class, 'store'])->name('registro.post');
-Route::post('/parroquias', [RegistroController::class, 'parroquias'])->name('parroquias');
+Route::get('/registro', [RegistroController::class, 'create'])->name('registro')->middleware('guest');
+Route::post('/registro', [RegistroController::class, 'store'])->name('registro.post')->middleware('guest');
+Route::post('/parroquias', [RegistroController::class, 'parroquias'])->name('parroquias')->middleware('guest');
 
 //Reset de contraseña
-Route::get('/reset-password',[ResetPassword::class, 'create'])->name('reset');
-Route::post('/reset-password',[ResetPassword::class, 'store'])->name('reset.post');
-Route::get('/user-reset-password/{token}',[ResetPassword::class, 'resetPassUser'])->name('reset.pass.user');
-Route::post('/user-reset-password/{token}',[ResetPassword::class, 'resetPassUserPost'])->name('reset.pass.user.post');
+Route::get('/reset-password',[ResetPassword::class, 'create'])->name('reset')->middleware('guest');
+Route::post('/reset-password',[ResetPassword::class, 'store'])->name('reset.post')->middleware('guest');
+Route::get('/user-reset-password/{token}',[ResetPassword::class, 'resetPassUser'])->name('reset.pass.user')->middleware('guest');
+Route::post('/user-reset-password/{token}',[ResetPassword::class, 'resetPassUserPost'])->name('reset.pass.user.post')->middleware('guest');
 
 
 
