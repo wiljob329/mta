094c96de7c14c0eaafacd265336e280950908f0e
Wilfredo Murillo <wilmugo@gmail.com>
2023-09-07 22:10:58 +0000

    eliminada prueba de rendimiento

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -10,7 +10,6 @@
   <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<meta name="loadforge-site-verification" content="1641617535aa902fef47337c95287d1b1ca6e004d7cae670cee308fb6b59d51fedc51df92dab3008b4d3099cb1f49ba32acf361a30d15f096cef391583f353b5" />
 
   {{-- <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" /> --}}
   <link rel="preconnect" href="https://fonts.googleapis.com">
